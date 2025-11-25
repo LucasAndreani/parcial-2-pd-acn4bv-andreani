8425ef4c5c20b68e79cc5b50e01f159bff23552b
Lucas Andreani <lucas.andreani@icloud.com>
2025-11-25 02:32:44 -0300

    feat(backend): GET y POST endpoints con persistencia JSON

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
+import fs from "fs";
 import express from "express";
 import cors from "cors";
 
 const app = express();
 const PORT = 3000;
+
+const DATA_PATH = "./data.json";
+
+function readData() {
+    const json = fs.readFileSync(DATA_PATH, "utf8")
+    return JSON.parse(json);
+}
+
+function writeData(data) {
+    fs.writeFileSync(DATA_PATH, JSON.stringify(data, null, 2))
+}
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +22,25 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.json({ message: "Servidor funcionando" });
 });
+
+app.get("/tasks", (req, res) => {
+    const tasks = readData();
+    res.json(tasks)
+});
+
+app.post("/tasks", (req, res) => {
+    const tasks = readData();
+
+    const newTask = {
+        id: Date.now(),
+        title: req.body.title
+    };
+
+    tasks.push(newTask);
+    writeData(tasks);
+
+    res.status(201).json(newTask);
+})
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
